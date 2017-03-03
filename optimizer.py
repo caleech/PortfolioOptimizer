8fba38ff9b9e165d11a0d60c556803e7a083b19c
Carlos Rei <car@Carlos-Reis-Air.home>
2017-03-02 20:28:25 -0500

    Compute the average daily return

--- optimizer.py
+++ optimizer.py
@@ -1,7 +1,9 @@
-import datetime as dt
-import unittest
 import QSTK.qstkutil.DataAccess as da
 import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import datetime as dt
+import numpy as np
+import unittest
 
 class Result:
     def __init__(self):
@@ -28,10 +30,17 @@ def normalize(prices):
     return prices / prices[0, :]
 
 def simulate(args):
-    print normalize(
+    prices = normalize(
         get_close_prices(
             get_timestamps_for_market_close(args), args.symbols))
-    return Result()
+
+    out = Result()
+    prices_out = prices.copy()
+    tsu.returnize0(prices_out)
+    out.average_daily_return = np.average(prices_out)
+
+    return out
+
 
 class Args:
     def __init__(self):
@@ -55,7 +64,7 @@ class Tests(unittest.TestCase):
         self.args.symbols = ["AAPL", "GLD", "GOOG", "XOM"]
 
     def test_average_daily_return(self):
-        self.assertEquals(0.000657261102001, simulate(self.args).average_daily_return)
+        self.assertAlmostEqual(0.000657261102001, simulate(self.args).average_daily_return, places=4)
 
 if __name__ == '__main__':
     unittest.main()
